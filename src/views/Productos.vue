a89cabe3f2ad41c04c5bc109fa3b873f48f33fa3
Eduardo <choboellobo@gmail.com>
2020-11-28 14:16:01 +0100

    discount app

--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -11,18 +11,22 @@
     <!-- Productos -->
     <div class="products">
       <div class="product" v-for="(p, index) in productos" :key="index">
+        <img style="width:100%"  v-if="p.producto.icono" :src="p.producto.icono" alt="">
         <div class="content">
+          <div class="discount" v-if="p.discount">{{ p.discount }} %</div>
           <h3 class="titulo">{{ p.producto.nombre}}</h3>
           <p class="descripcion" v-if="p.producto.descripcion">{{ p.producto.descripcion }}</p>
          
           <div class="precio">
             <span>
               <small v-if="p.precio_mitad">Ración</small>
-              {{ currency(p.precio *  (100 - categoria.descuento) / 100) }}
+              <div v-if="p.discount || categoria.descuento" class="antes">{{ currency(p.precio) }}</div>
+              {{ currency(p.precio *  (100 - checkDiscount(categoria.descuento, p.discount) ) / 100) }}
             </span>
             <span class="mitad" v-if="p.precio_mitad"> 
               <small>Media</small>
-              {{ currency(p.precio_mitad *  (100 - categoria.descuento) / 100) }}
+              <div v-if="p.discount || categoria.descuento" class="antes">{{ currency(p.precio) }}</div>
+              {{ currency(p.precio_mitad *  (100 -  checkDiscount(categoria.descuento, p.discount) ) / 100) }}
             </span>
           </div>
 
@@ -58,8 +62,13 @@ export default {
       })
       return formatter.format(price)
     }
+    const checkDiscount = (category, product) => {
+      if(product) return product
+      if(category) return category
+      return 0;
+    }
     console.log(productos)
-    return {productos, categoria, currency}
+    return {productos, categoria, currency, checkDiscount}
   },
 };
 </script>
@@ -101,6 +110,29 @@ export default {
   box-shadow: 1px 1px 8px rgba(grey, .5);
   margin-bottom: 1em;
   .content {
+      position: relative;
+    .discount {
+          position: absolute;
+          width: 75px;
+          height: 75px;
+          background: black;
+          color: white;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          font-size: 25px;
+          border-radius: 50%;
+          top: -50px;
+          right: -25px;
+          transform: translateX(-50%);
+          font-weight: bold;
+          &::after {
+            content: 'Oferta';
+            position: absolute;
+            top: 12px;
+            font-size: 14px;
+          }
+    }
     .titulo { 
       font-family: 'Open Sans', sans-serif;
       color: #C85B32;
@@ -110,6 +142,13 @@ export default {
       font-size: 12px;
     }
     .precio {
+      .antes {
+        display: inline;
+        text-decoration: line-through;
+        color: gray;
+        font-size: 20px;
+        font-weight: lighter;
+      }
       small { 
         position: absolute;
         bottom: -10px;
